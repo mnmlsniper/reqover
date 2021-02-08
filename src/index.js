e1c2a7772bab843710db94b6c6fb7a9ddeea0d7f
SergeyPirogov <serhii.pirogov@gmail.com>
2021-02-08 14:21:45 +0000

    fix arrow function

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const httpProxy = require('http-proxy');
 
 const proxy = httpProxy.createProxyServer({});
-http.createServer(function(req, res) {
+http.createServer((req, res) => {
   console.log('Request', req.method, req.url);
   proxy.web(req, res, { target: `http://httpbin.org/get?answer=42` });
 }).listen(3000);
