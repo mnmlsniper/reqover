ab26f9f1690ee19dd979f45b159f096908bd813c
SergeyPirogov <serhii.pirogov@gmail.com>
2021-02-08 16:15:11 +0200

    init

---
+++ .devcontainer/index.js
@@ -0,0 +1,8 @@
+const http = require('http');
+const httpProxy = require('http-proxy');
+
+const proxy = httpProxy.createProxyServer({});
+http.createServer(function(req, res) {
+  console.log('Request', req.method, req.url);
+  proxy.web(req, res, { target: `http://httpbin.org/get?answer=42` });
+}).listen(3000);
