7e0035fdfea850b337aa71b809c1effe13eeb78a
SergeyPirogov <serhii.pirogov@gmail.com>
2021-02-09 13:54:25 +0000

    move to express

--- src/index.js
+++ src/index.js
@@ -1,8 +1,34 @@
-const http = require('http');
-const httpProxy = require('http-proxy');
+import express from 'express';
+import morgan from "morgan";
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
-const proxy = httpProxy.createProxyServer({});
-http.createServer((req, res) => {
-  console.log('Request', req.method, req.url);
-  proxy.web(req, res, { target: `http://httpbin.org/get?answer=42` });
-}).listen(3000);
+// Create Express Server
+const app = express();
+
+// Configuration
+const PORT = 3000;
+const HOST = "localhost";
+const API_SERVICE_URL = "https://petstore.swagger.io/v2/";
+
+// Logging
+app.use(morgan('dev'));
+
+// Info GET endpoint
+app.get('/info', (req, res, next) => {
+  res.send('This is a proxy service which proxies to Billing and Account APIs.');
+});
+
+
+// Proxy endpoints
+app.use('/', createProxyMiddleware({
+  target: API_SERVICE_URL,
+  changeOrigin: true,
+  pathRewrite: {
+      [`^/`]: '',
+  },
+}));
+
+// Start the Proxy
+app.listen(PORT, HOST, () => {
+  console.log(`Starting Proxy at ${HOST}:${PORT}`);
+});
