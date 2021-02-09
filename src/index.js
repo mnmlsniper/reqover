10fd5294a1a4f1eee1946174efc290a82f68c2ed
SergeyPirogov <serhii.pirogov@gmail.com>
2021-02-09 14:49:52 +0000

    add pattern

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import {
   anOpenApiSpec,
   OpenApiSpecBuilder,
 } from '@loopback/openapi-spec-builder';
-
+import swaggerParser from 'swagger-parser';
+import UrlPattern from 'url-pattern'
 // Create Express Server
 const app = express();
 const spec = anOpenApiSpec()
@@ -23,6 +24,26 @@ app.get('/info', (req, res, next) => {
   res.send(spec.build());
 });
 
+app.get('/original',async (req, res) => {
+  const original = await swaggerParser.validate("https://petstore.swagger.io/v2/swagger.json");
+  const actual = spec.build();
+
+  const originalPaths = original.paths
+  const actualPaths = actual.paths
+
+  const options = {
+    optionalSegmentStartChar : '{',
+    optionalSegmentEndChar : '}',
+    segmentNameStartChar : ''
+  }
+
+  var pattern = new UrlPattern('/pet/{petId}', options);
+
+  const match = pattern.match('/pet/1')
+
+  console.log()
+  res.send(actualPaths)
+})
 
 function proxyReq(proxyReq, req, res) {
   // add custom header to request
