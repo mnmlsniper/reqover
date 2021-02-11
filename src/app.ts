df917b4e217ecba86a33ae76969eeaad59ef8e35
Alexey Panashchenko <panashchenko89@gmail.com>
2021-02-11 21:26:12 +0200

    Add basic exaple with TS

---
+++ src/app.ts
@@ -0,0 +1,92 @@
+import express from 'express';
+import helmet from 'helmet';
+import hpp from 'hpp';
+import morgan from 'morgan';
+import compression from 'compression';
+import {logger} from './utils/logger';
+import {createProxyMiddleware} from 'http-proxy-middleware';
+
+const API_SERVICE_URL = 'https://petstore.swagger.io';
+
+export const spec = [];
+
+class App {
+    public app: express.Application;
+    public port: string | number;
+    public env: string;
+
+    constructor(routes: any[]) {
+        this.app = express();
+        this.app.set('view engine', 'ejs');
+        this.port = process.env.PORT || 3000;
+        this.env = process.env.NODE_ENV || 'development';
+
+        this.initializeMiddlewares();
+        this.initializeRoutes(routes);
+
+        this.app.use(
+            '/',
+            createProxyMiddleware({
+                target: API_SERVICE_URL,
+                changeOrigin: true,
+                pathRewrite: {
+                    [`^/`]: '',
+                },
+                onProxyReq: proxyReq,
+                onProxyRes: proxyRes,
+            }),
+        );
+    }
+
+    public listen() {
+        this.app.listen(this.port, () => {
+            logger.info(`🚀 App listening on the port ${this.port}`);
+        });
+    }
+
+    public getServer() {
+        return this.app;
+    }
+
+    private initializeMiddlewares() {
+        if (this.env === 'production') {
+            this.app.use(morgan('combined'));
+        } else if (this.env === 'development') {
+            this.app.use(morgan('dev'));
+        }
+        this.app.use(hpp());
+        this.app.use(helmet());
+        this.app.use(compression());
+        this.app.use(express.json());
+        this.app.use(express.urlencoded({extended: true}));
+    }
+
+    private initializeRoutes(routes: any[]) {
+        routes.forEach((route) => {
+            this.app.use('/', route.router);
+        });
+    }
+}
+
+const proxyReq = (proxyReq, req, next) => {
+    // add custom header to request
+    // or log the req
+};
+
+const proxyRes = (proxyRes, req, res) => {
+    const method = req.method;
+    const responseStatus = `${proxyRes.statusCode}`;
+    const path = req.originalUrl.split('?')[0];
+    const params = req.query;
+    const queryParameters = Object.entries(params).map(([p, v]) => {
+        return {name: p, value: v};
+    });
+
+    spec.push({
+        path: path,
+        method: method,
+        response: responseStatus,
+        parameters: queryParameters,
+    });
+};
+export default App;
---
+++ src/controllers/swagger.controller.ts
@@ -0,0 +1,141 @@
+import {NextFunction, Request, Response} from 'express';
+import swaggerParser from '@apidevtools/swagger-parser';
+import UrlPattern from 'url-pattern';
+import {spec} from '../app';
+
+class AuthController {
+    private readonly swaggerUrl = 'https://petstore.swagger.io/v2/swagger.json';
+
+    public specs = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+        try {
+            res.send(spec);
+        } catch (error) {
+            next(error);
+        }
+    };
+
+    public info = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+        try {
+            const apiList = await this.getSwaggerPaths(this.swaggerUrl);
+            res.send(apiList);
+        } catch (error) {
+            next(error);
+        }
+    };
+
+    public report = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+        try {
+            const reportData = await this.getCoverageReport();
+            res.render('index', {data: reportData});
+        } catch (error) {
+            next(error);
+        }
+    };
+
+    public coverage = async (req: any, res: Response, next: NextFunction): Promise<void> => {
+        try {
+            res.send(await this.getCoverageReport());
+        } catch (error) {
+            next(error);
+        }
+    };
+
+    private getSwaggerPaths = async (swaggerSpec: any) => {
+        const swaggerInfo: any = await swaggerParser.validate(swaggerSpec);
+        const basePath = swaggerInfo.basePath;
+        const apiPaths = Object.entries(swaggerInfo.paths);
+
+        const apiList = apiPaths.map(([apiPath, value]) => {
+            const methods = Object.entries(value).map(([methodName, data]) => {
+                return {
+                    path: `${basePath}${apiPath}`,
+                    name: methodName.toUpperCase(),
+                    responses: Object.keys(data.responses),
+                    parameters: data.parameters,
+                };
+            });
+
+            return {
+                path: `${basePath}${apiPath}`,
+                methods: methods,
+            };
+        });
+
+        return apiList;
+    };
+
+    private getCoverageReport = async () => {
+        const apiList = await this.getSwaggerPaths('https://petstore.swagger.io/v2/swagger.json');
+
+        const apiCovList: any = apiList.map((apiItem) => {
+            const coveredApis = this.findCoveredApis(apiItem);
+
+            const coveredMethods = apiItem.methods.map((method) => {
+                const coveredMethods: any = coveredApis.filter((c) => c.method == method.name);
+                const coveredStatusCodes = [...new Set(coveredMethods.map((m) => m.response))];
+                const missingStatusCodes = method.responses.filter((s) => !coveredStatusCodes.includes(s));
+
+                const coveredParameters = coveredMethods
+                    .map((m) => {
+                        return m.parameters.map((p) => p.name);
+                    })
+                    .flat();
+                const missingParameters = method.parameters
+                    .map((p) => {
+                        return {
+                            name: p.name,
+                            required: p.required,
+                            in: p.in,
+                            type: p.type,
+                        };
+                    })
+                    .map((mp) => mp.name)
+                    .filter((m) => !coveredParameters.includes(m));
+
+                const coverage = ((coveredStatusCodes.length / (coveredStatusCodes.length + missingStatusCodes.length)) * 100).toFixed();
+
+                let status = 'danger';
+                if (+coverage > 0 && +coverage < 100) {
+                    status = 'warning';
+                } else {
+                    status = 'success';
+                }
+
+                return {
+                    path: apiItem['path'],
+                    method: method.name,
+                    coverage: coverage,
+                    status: status,
+                    responses: {
+                        missed: missingStatusCodes,
+                        covered: coveredStatusCodes,
+                    },
+                    parameters: {
+                        missed: missingParameters,
+                        covered: coveredParameters,
+                    },
+                };
+            });
+
+            return coveredMethods;
+        });
+
+        return apiCovList.flat();
+    };
+
+    private regExMatchOfPath = (apiPath: string, rPath: string) => {
+        const pattern = new UrlPattern(apiPath.replace(/\/{/g, '{/:'), {
+            optionalSegmentStartChar: '{',
+            optionalSegmentEndChar: '}',
+        });
+        return pattern.match(rPath);
+    };
+
+    private findCoveredApis = (apiItem: any) => {
+        return spec.filter((path) => {
+            return this.regExMatchOfPath(apiItem['path'], path['path']);
+        });
+    };
+}
+
+export default AuthController;
